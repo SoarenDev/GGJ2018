e8865dcbd94a306e6b241a3780cbdfcc79dd206d
ngUpsiLoN <t.ledieu@rubika-edu.com>
2018-01-27 00:20:32 +0100

    Premier test

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -6,7 +6,7 @@ public class test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		print("test wess;k encore");
+		print("test wess;k encore cébon");
 	}
 	
 	// Update is called once per frame
