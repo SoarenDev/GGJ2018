34f4999af0a0c633af8838d48450a1592e4f87d0
Gyotek <a.herbreteau@rubika-edu.com>
2018-01-27 22:22:32 +0100

    Foule new version

--- Assets/Scripts/Foule.cs
+++ Assets/Scripts/Foule.cs
@@ -6,14 +6,14 @@ using UnityEngine;
 
 public class Foule : MonoBehaviour {
 
-	public Dancer d;
-
+	public GameManager GM;
 	public GameObject Moogle;
 	public Transform foule;
 
-	private int nbDancer = 1;
+	private int nbDancer = 0;
 	private float posX;
 	private float posY;
+	private float circleIncrease = 0.005f;
 
 	// Use this for initialization
 	void Start () {
@@ -22,28 +22,48 @@ public class Foule : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		GM.player.GetComponent<PlayerController> ().speed = 0.2f + (nbDancer * 0.0005f);
+
+
 		if (Input.GetKey (KeyCode.Space))
 			SpawnDancer ();
 	}
 
 	private void generateSpawnPosition () {
-		posX = Random.Range(foule.position.x - (foule.GetComponent<CircleCollider2D>().radius * 5), foule.position.x + (foule.GetComponent<CircleCollider2D>().radius * 5));
-		posY = Random.Range(foule.position.y - (foule.GetComponent<CircleCollider2D>().radius * 5), foule.position.y + (foule.GetComponent<CircleCollider2D>().radius * 5));
-	
-		//min -> posFoule + espace des dancers autour
-		//max -> posFoule max + espace des dancers autour
 
-
+		switch(Random.Range(1,5)){
+		case 1:
+			posX = Random.Range (foule.position.x - (foule.GetComponent<CircleCollider2D> ().radius * 7.5f), foule.position.x - (foule.GetComponent<CircleCollider2D> ().radius * 10));
+			posY = Random.Range (foule.position.y - (foule.GetComponent<CircleCollider2D> ().radius * 10), foule.position.y + (foule.GetComponent<CircleCollider2D> ().radius * 10));
+			break;
+		case 2:
+			posX = Random.Range (foule.position.x + (foule.GetComponent<CircleCollider2D> ().radius * 7.5f), foule.position.x + (foule.GetComponent<CircleCollider2D> ().radius * 10));
+			posY = Random.Range (foule.position.y - (foule.GetComponent<CircleCollider2D> ().radius * 10), foule.position.y + (foule.GetComponent<CircleCollider2D> ().radius * 10));
+			break;
+		case 3:
+			posX = Random.Range (foule.position.x - (foule.GetComponent<CircleCollider2D> ().radius * 7.5f), foule.position.x + (foule.GetComponent<CircleCollider2D> ().radius * 10));
+			posY = Random.Range (foule.position.y - (foule.GetComponent<CircleCollider2D> ().radius * 10), foule.position.y - (foule.GetComponent<CircleCollider2D> ().radius * 10));
+			break;
+		case 4:
+			posX = Random.Range (foule.position.x - (foule.GetComponent<CircleCollider2D> ().radius * 7.5f), foule.position.x + (foule.GetComponent<CircleCollider2D> ().radius * 10));
+			posY = Random.Range (foule.position.y + (foule.GetComponent<CircleCollider2D> ().radius * 10), foule.position.y + (foule.GetComponent<CircleCollider2D> ().radius * 10));
+			break;
+		}
 	}
 
 
 
 	private void SpawnDancer () {
+
+
+		circleIncrease *= 0.996f;
+		foule.GetComponent<CircleCollider2D> ().radius += circleIncrease;
 		nbDancer++;
 		generateSpawnPosition ();
-		GameObject dancerSpawned = Instantiate (Moogle, new Vector2(posX, posY), foule.rotation);
-		d = dancerSpawned.GetComponent<Dancer> ();
+		GameObject dancerSpawned = Instantiate (Moogle, new Vector2(posX, posY), foule.rotation, foule.transform);
+
+		//d = dancerSpawned.GetComponent<Dancer> ();
 		//d.foule = this;
 	}
-}
+	}
