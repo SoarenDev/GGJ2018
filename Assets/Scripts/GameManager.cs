da11dbcb5c58d166aceb928d288f1ecf0b7a15a7
ngUpsiLoN <t.ledieu@rubika-edu.com>
2018-01-27 01:54:52 +0100

    VERSION DE BASE - PLAYER ET GAMEMANAGER

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+[Space(10)][Header("Scripts binding")]
+	public		GameManager		gameManager;
+
+[Space(10)][Header("Objects bindings")]
+	public		GameObject		player;
+
+[Space(10)][Header("Usual variable")]
+	public		string			test;
+
+	void Start () 
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+
+	void Update () {
+		
+	}
+}
